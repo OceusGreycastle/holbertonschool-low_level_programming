19d3033c37372a1cd0669b15d48ba2c648d5f003
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-22 10:58:03 -0700

    last digit betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	printf("Last digit of the number %d is %d \n", n, m);
 	if (n > 5)
 	{
-		printf("%d and is greater than 5\n", n);
+		printf("and is greater than 5\n", n);
 	}
 	else if (n < 6 && n != 0)
 	{
-		printf("%d and is less than 6 and not 0\n", n);
+		printf("and is less than 6 and not 0\n", n);
 	}
 	else
 	{
-		printf("%d and is zero\n", n);
+		printf("and is zero\n", n);
 	}
 	return (0);
 }
