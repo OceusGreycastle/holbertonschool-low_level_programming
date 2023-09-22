69c47097f45f3a4823912363a55dbd6f7f279e16
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-22 11:47:04 -0700

    no q or e

--- variables_if_else_while/4-print_alphabet.c
+++ variables_if_else_while/4-print_alphabet.c
@@ -6,18 +6,18 @@
 */
 int main(void)
 {
-        char letter = 'a';
+	char letter = 'a';
 
-        while (letter <= 'z')
-        {
+	while (letter <= 'z')
+	{
 		if (letter != 'q' && letter != 'e')
 		{
-                	putchar(letter);
+			putchar(letter);
 		}
-                letter++;
-        }
+		letter++;
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
