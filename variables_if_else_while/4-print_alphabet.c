23dd330373522d8a1ca2cf197fceccdcf1b22da8
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-22 11:44:55 -0700

    No q or e

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - prints a specific line
+*
+* Return: 0
+*/
+int main(void)
+{
+        char letter = 'a';
+
+        while (letter <= 'z')
+        {
+		if (letter != 'q' && letter != 'e')
+		{
+                	putchar(letter);
+		}
+                letter++;
+        }
+
+        putchar('\n');
+
+        return (0);
+}
