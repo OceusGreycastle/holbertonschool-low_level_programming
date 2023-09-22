53b8ad21241c211fefdd718a3d63d99ed403deca
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-22 11:02:11 -0700

    last digit betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	m = n % 10;
-	printf("Last digit of the number %d is %d", n, m);
+	printf("Last digit of the number %d is %d ", n, m);
 	if (n > 5)
 	{
 		printf("and is greater than 5\n");
