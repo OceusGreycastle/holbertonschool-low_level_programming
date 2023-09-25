ede2d00b6783c87128147bc9292966229861aa01
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-24 23:59:56 -0700

    Last digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,18 @@ int print_last_digit(int n)
 {
 	int a;
 
+	if (n == -2147483647 - 1)
+	{
+		_putchar('8');
+		return (8);
+	}
+	if (n < 0)
+	{
+		n = -n;
+	}
+
 	a = n % 10;
+
 	_putchar(a + '0');
 	return (a);
 }
