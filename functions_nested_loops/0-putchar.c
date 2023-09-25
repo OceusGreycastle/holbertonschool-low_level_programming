b1078474e39a5c4e3c9fe0ac161fa5dd6b0457da
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-24 17:23:35 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - prints a specific line
+* Return: 0
+*/
+int main(void)
+{
+	char c[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8 ; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar("\n");
+
+	return (0);
+}
