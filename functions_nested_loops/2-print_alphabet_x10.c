99d3c6c422e75b40d6c0206929db2b55771f2d30
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-24 18:56:40 -0700

    test

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,14 @@
 #include "main.h"
+
 /**
 * print_alphabet_x10 - prints a specific line
 * Return: 0
 */
+
 void print_alphabet_x10(void)
 {
 	char letter = 'a';
+	int i;
 
 	for (i = 0; i < 10 ; i++)
 	{
