8d502aeddd57001aa64863059215f6033b9d6399
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-09-28 06:43:41 -0700

    Line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,22 @@
 #include "main.h"
 /**
  * print_line - function
- * @n: is the integer for the line function
+ *@n: an int value
  * Return: 0
  */
+void print_line(int n)
+{
+	int a = 0;
 
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+	 _putchar('\n');
+	}
+	else
+	_putchar('\n');
+}
