997b05458d8959e5ff5ecd6ea845282e4afe7736
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-01 09:00:47 -0700

    FIZZBUZZ

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,22 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
-			printf("Fizz");
-		if (i % 5 == 0)
-			printf("Buzz");
-		if ((i % 3 != 0) && (i % 5 != 0))
-			printf("FizzBuzz");
-		printf("\n");
+		{
+			printf('Fizz\n');
+		}
+		else if (i % 5 == 0)
+		{
+			printf('Buzz\n');
+		}
+		else if ((i % 3 != 0) && (i % 5 != 0))
+		{
+			printf('FizzBuzz\n');
+		}
+		else
+		{
+			printf('i\n');
+		}
+		printf('\n');
 	}
 
 	return (0);
