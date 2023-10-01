d99a2e994dcba5e2bc8528e03fd387337fde7e07
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-01 13:15:46 -0700

    TRIANGULAR

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -2,10 +2,31 @@
 
 /**
 *print_triangle - prints triangles
-*
-*
+*@size: SIZE
+*Return: Always 0
 */
 
 void print_triangle(int size)
 {
-	
+	int i = 1;
+	int hash = 1;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (hash = 1; hash <= (size - i); hash++)
+		{
+			_putchar(' ');
+		}
+		for (hash = 1; hash <= i; hash++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
