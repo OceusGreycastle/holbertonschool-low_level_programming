6e45f2330773b7a5945778125f0f0142ae3f6083
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-01 08:16:25 -0700

    FIZZBUZZ

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
 /**
-*
+*main - a variable
 *
 *
 */
 
+int main(void)
+{
+	int i;
+	for (i = 1; i <= 100; i++;)
 
+	{
+		if (((i % 3) || (i % 5)) == 0)
+			printf("number = %d FizzBuzz\n", i);
+		else if ((i % 3) == 0)
+			printf("number = %d Fizz\n", i);
+		else if ((i % 5) == 0)
+			printf("number = %d Buzz\n", i);
+		else
+			printf("number = %d\n", i);
+	}
+}
