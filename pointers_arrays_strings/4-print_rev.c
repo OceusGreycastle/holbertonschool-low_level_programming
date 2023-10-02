106ac7a01aa840dc0cd5238744bdab57a8bd6cba
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-02 08:39:19 -0700

    Reverse Print

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_rev - prints in reverse
+*@s: VARIABLE
+*/
+
+void print_rev(char *s)
+{
+	int E = 0;
+
+	while (E[s] != '\0')
+	{
+		_putchar(E[s]);
+		E++;
+	}
+	_putchar('\n');
+}
