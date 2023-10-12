a02f191bb7d1376118eea811cc16900c9ac821cf
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-12 07:12:44 -0700

    Print String

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,11 +1,20 @@
 #include "main.h"
 
 /**
-*
-*
+*_puts_recursion - Prints recursively off a given input
+*@s: pointer to the string input
 *
 */
 
 void _puts_recursion(char *s)
 {
-	
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(++s);
+	}
+}
