1b9d69b8a9a3a9078c01192837e50c1cb3f3db19
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-30 14:45:44 -0500

    fixes

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -6,7 +6,7 @@
 /**
  * new_dog -
 */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *puppy = malloc(sizeof(dog_t));
 	char *namecopy = strdup(name);
