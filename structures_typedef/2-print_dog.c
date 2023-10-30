e9fb5ae4310da607bb369b7e1b68e01d94b092e3
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-10-30 14:21:04 -0500

    fixed my dumbdumb

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Name: %f\n", (*d).name);
+			printf("Name: %s\n", (*d).name);
 		}
 		printf("Age: %f\n", (*d).age);
 		if(!(*d).owner)
@@ -25,7 +25,7 @@ void print_dog(struct dog *d)
 		}
 		else
 		{
-			printf("Owner: %f\n", (*d).owner);
+			printf("Owner: %s\n", (*d).owner);
 		}
 	}
 }
