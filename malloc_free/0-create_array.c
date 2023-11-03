5cedf8f509435990405c63872b34c4de85e19f21
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-03 09:02:36 -0500

    fix

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -6,21 +6,21 @@ char *create_array(unsigned int size, char c)
 {
 	char *newArr;
 	int i;
-	newArr = malloc(size + 1)
+	newArr = malloc(size + 1);
 
-	if(newArr == NULL)
+	if (newArr == NULL)
 	{
 		return;
 	}
-	if(size == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
 	for(i = 0; i < size; i++)
 	{
-		newArr[i] = c
+		newArr[i] = c;
 	}
-		newArr[i] = '\0'
+		newArr[i] = '\0';
 		free(newArr);
 		return(newArr);
 }
