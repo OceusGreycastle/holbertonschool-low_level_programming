8059d9d5987efc213e80ea8bc3e2d17f40cdbe4c
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-03 22:34:14 -0500

    Fixed NEW DOGE

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <string.h>
+#include "_strlen.c"
+#include "strcpy.c"
 
 /**
  * new_dog - Makes a new dog
@@ -13,22 +15,28 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
-	char *namecopy = strdup(name);
-	char *ownercopy = strdup(owner);
 
 	if (my_dog == NULL)
-	return (NULL);
+		return (NULL);
 
-	if (my_dog && ownercopy && namecopy)
+	(*my_dog).name = malloc(_strlen(name) + 1);
+	if (my_dog->name == NULL)
 	{
-		(*my_dog).name = namecopy;
-		(*my_dog).owner = ownercopy;
-		(*my_dog).age = age;
-		return (my_dog);
+		free(my_dog);
+		return (NULL);
 	}
 
-	free(my_dog->name);
-	free(my_dog->owner);
-	free(my_dog);
-	return (0);
+	(*my_dog).owner = malloc(_strlen(owner) + 1);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog);
+		free(my_dog->name);
+		return (NULL);
+	}
+
+	strcpy(my_dog->name, name);
+	my_dog->age = age;
+	strcpy(my_dog->owner, owner);
+
+	return (my_dog);
 }
