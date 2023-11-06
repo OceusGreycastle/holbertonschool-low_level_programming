0dc808539ad74ef876b6546b81ff2db8fc5648eb
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-06 03:14:17 -0600

    minor changes

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,7 +11,7 @@ int (*get_op_func(char *s))(int, int)
 		{"*", op_mul},
 		{"/", op_div},
 		{"%", op_mod},
-		{NULL, NULL},
+		{NULL, NULL}
 	};
 
 	int i;
@@ -20,7 +20,7 @@ int (*get_op_func(char *s))(int, int)
 
 	while (i < 5)
 	{
-		if (strcmp(s, ops[i].op) == 0)
+		if (strcmp(s, ops[i].op)== 0)
 		{
 			return (ops[i].f);
 		}
