4b2dc4d18c08331c1baba234cb401979da2a64e9
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-06 01:34:52 -0600

    iterated through an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - interates in an array
+ * @array: iterable array
+ * @size: array size
+ * @action: function being called upon
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i = 0;
+	int s = (int)size;
+
+	if (array == NULL || action == NULL)
+	{
+		return ;
+	}
+	while (i < s)
+	{
+		(*action)(array[i]);
+		i++;
+	}
+}
