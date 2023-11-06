f068c34bb61e511431e24c70f866c3c0188e907b
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-06 12:09:04 -0600

    spacing

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,44 +6,44 @@
 /**
  * print_all - prints any input
  * @format: list of args
- * 
+ *
 */
 
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int x = 0;
-    char *str;
+	va_list args;
+	int x = 0;
+	char *str;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[x])
-    {
-        switch(format[x])
-        {
-        case 'c':
-            printf("%c", va_arg(args, int));
-            break;
-        case 'i':
-            printf("%d", va_arg(args, int));
-            break;
-        case 'f':
-            printf("%f", va_arg(args, double));
-            break;
-        case 's':
-            str = va_arg(args, char *);
-            if (!str)
-            {
-                printf("(nil)");
-            }
-            printf("%s", str);
-            break;
-        default:
-            break;
-        }
-        x++;
-    }
-    printf("\n");
+	while (format && format[x])
+	{
+		switch(format[x])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			str = va_arg(args, char *);
+			if (!str)
+			{
+				printf("(nil)");
+			}
+			printf("%s", str);
+			break;
+		default:
+			break;
+		}
+		x++;
+	}
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
