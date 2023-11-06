65736c4c86fb48448ba889ac158f44b4f78ba525
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-06 03:01:06 -0600

    calc finished

---
+++ function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+int main(int argc, char **argv)
+{
+	int (*proc_nums)(int, int);
+
+	proc_nums = get_op_func(argv[2]);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else if (proc_nums)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	else
+	{
+		printf("%d\n", proc_nums(atoi(argv[1]), atoi(argv[3])));
+		return (0);
+	}
+}
