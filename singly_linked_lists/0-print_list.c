5a2af0270d00079eb2403b8101161b4fde76234c
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-12 19:19:31 -0600

    fixed mistake

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -10,16 +10,18 @@ size_t print_list(const list_t *h)
 {
 	int cnt = 0;
 
-	if ((*h).str == NULL)
+	while ((*h).str != NULL)
 	{
-		printf("[0] (nil)");
+		if ((*h).str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", (*h).len, (*h).str);
+		}
+		h = (*h).next;
+		cnt++;
 	}
-	else
-	{
-		printf("[%d] %s\n", (*h).len, (*h).str);
-	}
-	h = (*h).next;
-	cnt++;
-
 	return (cnt);
 }
