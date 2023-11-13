8bd4fbd466e2738e483fd538b64b40e635f52c09
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-13 10:42:21 -0600

    List length

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,7 +3,14 @@
 
 size_t list_len(const list_t *h)
 {
-    size_t count;
+	size_t count;
 
-    count = 0;
+	count = 0;
+
+	while (h != NULL)
+	{
+		h = (*h).next;
+		count++;
+	}
+	return (count);
 }
