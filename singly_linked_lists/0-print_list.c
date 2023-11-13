8af75e452ad58abc323b2ed37ebb235cde468eb6
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-12 19:26:33 -0600

    maybe thinking wrong?

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,18 +12,15 @@ size_t print_list(const list_t *h)
 {
 	int cnt = 0;
 
-	while ((*h).str != NULL)
+	if ((*h).str == NULL)
 	{
-		if ((*h).str == NULL)
-		{
-			printf("[%d] %s\n", 0, "(nil)");
-		}
-		else
-		{
-			printf("[%d] %s\n", (*h).len, (*h).str);
-		}
-		h = (*h).next;
-		cnt++;
+		printf("[%d] %s\n", 0, "(nil)");
 	}
+	else
+	{
+		printf("[%d] %s\n", (*h).len, (*h).str);
+	}
+	h = (*h).next;
+	cnt++;
 	return (cnt);
 }
