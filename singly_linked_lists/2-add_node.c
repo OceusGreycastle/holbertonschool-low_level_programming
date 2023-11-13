a38473e5c2c2f6929507f6cf15f4171200cb34e1
OceusGreycastle <ttvprojectmisfit@gmail.com>
2023-11-13 12:25:28 -0600

    added node code lol

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -3,5 +3,12 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    
+	list_t* new_node
+	= (list_t*)malloc(sizeof(list_t));
+
+	new_node->str = (*str);
+
+	new_node->next = (*head);
+
+	(*head) = new_node;
 }
